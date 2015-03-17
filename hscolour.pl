2abc347e07b1351c75d3875ae60de13e181fcdc5
Ricky Elrod <ricky@elrod.me>
2015-03-17 00:22:18 -0400

    with comments and a patch
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- hscolour.pl
+++ hscolour.pl
@@ -1,5 +1,8 @@
 # (c) 2015 Ricky Elrod <relrod@redhat.com>, released under GPL
 # (same as HsColour itself).
+#
+# NOTE: As of this writing, this requires patching HsColour to work correctly.
+# See hscolour.patch in the GitHub repository from which this script was found.
 
 use warnings;
 use strict;
